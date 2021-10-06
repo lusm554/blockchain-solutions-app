d2d5969498cdd4f44bbf689d3cc30de5c5af4ba5
loveyousomuch <loveyousomuch554@gmail.com>
2021-10-06 10:03:46 +0300

    the ability to run the application in development and production mode

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM python:3.9-alpine
 WORKDIR /src
-ENV FLASK_APP=main.py
-ENV FLASK_RUN_HOST=0.0.0.0
-ENV FLASK_RUN_PORT=80
+ENV QUART_APP=main:app
 RUN apk add --no-cach postgresql-dev gcc python3-dev musl-dev linux-headers postgresql-client
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -6,10 +6,15 @@ set -e
 host="$1"
 shift
   
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c "\q"; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 2
 done
-  
->&2 echo "Postgres is up - executing command"
-exec "$@"
+
+echo "Server starting in $QUART_ENV mode"
+if [ "$QUART_ENV" == "production" ]; then
+  hypercorn main:app -b 0.0.0.0:80
+  exit 0
+fi
+
+quart run --host 0.0.0.0 --port 80
